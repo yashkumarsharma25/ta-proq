d5e5f40df007a49b7bdba8eab6f5d0752f2b84f2
livinNector <livinnector2001@gmail.com>
2023-12-03 17:11:24 +0530

    added solution template

--- src/seek_filler/proq_gen.py
+++ src/seek_filler/proq_gen.py
@@ -1,14 +1,49 @@
 #!/usr/bin/env python
 import argparse
 
+yaml_header_sample="""---
+local_evaluate:
+    file_name: test.py
+    build: ""
+    run: python test.py
+lang : "Python3"
+# mm/dd/yyyy,HH:MM
+deadline : "09/28/2023,23:59"
+evaluator": "nsjail"
+ignore_presentation_error": true
+allow_compile": true
+show_sample_solution": true
+---
+"""
+
+solution_content="""
+```
+<prefix>
+
+</prefix>
+<template>
+
+<solution>
+
+</solution>
+</template>
+<suffix>
+
+</suffix>
+<suffix_invisible>
+
+</suffix_invisible>
+```
+"""
 
 def generate_template(output_file, num_problems, num_public, num_private, pattern):
     with open(output_file, "w") as file:
+        file.write(yaml_header_sample)        
         for problem_num in range(1, num_problems + 1):
             file.write(f"# Unit Name\n\n")
             file.write(f"## {pattern.format(problem_num)}\n\n")
             file.write(f"### Problem Statement\n\n")
-            file.write(f"### Solution\n```\n\n```\n\n")
+            file.write(f"### Solution{solution_content}\n")
             file.write(f"### Testcases\n\n")
 
             file.write(f"#### Public Testcases\n\n")
